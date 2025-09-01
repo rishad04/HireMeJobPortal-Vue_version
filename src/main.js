e2f91c06d8bd5567f45ca6678d44c3e44a5629d4
rishad04 <rishadhossain33@gmail.com>
2025-09-01 13:34:22 +0600

    main js

--- src/main.js
+++ src/main.js
@@ -1,18 +1,14 @@
 import { createPinia } from 'pinia';
 import 'sweetalert2/dist/sweetalert2.min.css';
 import { createApp } from 'vue';
-import apiClient from './api';
 import App from './App.vue';
 import './assets/main.css';
 import router from './router';
 
-apiClient.get('/sanctum/csrf-cookie').then(() => {
-    const app = createApp(App);
+const app = createApp(App)
 
-    app.use(createPinia());
-    app.use(router);
 
-    app.mount('#app');
-}).catch(error => {
-    console.error("Could not fetch CSRF cookie:", error);
-});
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
